fcb9111c03c26a09d9669e9e371243ed45744eca
Tyler Timpanaro <tyler.timpanaro@gmail.com>
2025-11-28 13:19:57 -0500

    fix: remove node_modules from tracking; add .github dir

--- node_modules/@testing-library/svelte/types/core/validate-options.d.ts
+++
@@ -1,5 +0,0 @@
-export class UnknownSvelteOptionsError extends TypeError {
-    constructor(unknownOptions: any, allowedOptions: any);
-}
-export function validateOptions(options: any): any;
-//# sourceMappingURL=validate-options.d.ts.map
